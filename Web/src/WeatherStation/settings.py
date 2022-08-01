4f2a409fb813fb2734e8a84ddb8dfbe40ac7d8f9
Baptiste Mercier <airdeon@gmail.com>
2022-08-01 22:55:34 +0200

    creation de la base css probleme de lien static

--- Web/src/WeatherStation/settings.py
+++ Web/src/WeatherStation/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["weatherhome.baptistemphoto.com"]
+ALLOWED_HOSTS = ["127.0.0.1","weatherhome.baptistemphoto.com"]
 
 
 # Application definition
@@ -117,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+print(STATIC_ROOT)
 STATIC_URL = "/static/"
+print(STATIC_URL)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
